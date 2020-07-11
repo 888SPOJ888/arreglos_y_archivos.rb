b78a36b642c9551bb5b791c00a13f41912749173
alexis Montealegre <ajmu26@gmail.com>
2020-07-11 12:11:53 -0400

    almost done

---
+++ calculo_notas2.rb
@@ -0,0 +1,13 @@
+data = File.open('notas.data.txt').readlines
+data.map!{|line| line.split(",")}
+
+
+def nota_mas_alta(grades)
+    results = []
+    grades.each do |student_max_grade|
+        results.push([student_max_grade[0], student_max_grade.map{|num| num.to_i}.max])
+   end
+   return results
+end
+
+pp nota_mas_alta(data)
